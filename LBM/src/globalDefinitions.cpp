84858752b0bfae77a9588556a2189a6250655978
Roland Guichard <r.guichard@ucl.ac.uk>
2016-11-08 15:40:09 +0000

    Add globalDefinitions.cpp to git

---
+++ LBM/src/globalDefinitions.cpp
@@ -0,0 +1,10 @@
+#include "globalDefinitions.h"
+#include<vector>
+
+using namespace std;
+
+
+void readLBMGeometriesFromFile (int &lx, int &ly, int &lz, int &nbDensities, const string fileName) {
+	vector<string> geometryParameters;
+
+}
