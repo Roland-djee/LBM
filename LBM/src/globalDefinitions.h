7c305d5729317eeaf7cb6ce4d26b6679d7ec291e
Roland Guichard <r.guichard@ucl.ac.uk>
2016-11-08 15:20:50 +0000

    Add globalDefinitions header to git.

---
+++ LBM/src/globalDefinitions.h
@@ -0,0 +1,7 @@
+/**
+ * Description: Header for global definitions.
+ * Created: 8 Nov. 2016
+ * Author: Roland Guichard
+ */
+
+void readLBMGeometriesFromFile (int &lx, int &ly, int &lz, int &nbDensities, const string fileName)
