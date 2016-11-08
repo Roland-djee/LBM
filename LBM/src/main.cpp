daaf137e1bab4617115c87b463c366024d307e21
Roland Guichard <r.guichard@ucl.ac.uk>
2016-11-08 14:53:11 +0000

    Add main.cpp to git.

---
+++ LBM/src/main.cpp
@@ -0,0 +1,25 @@
+//================================================
+// Name: LBM.cpp
+// Author: Roland Guichard
+// Version:
+// Copyright: L-GPL
+// Description: CUDA Lattice-Boltzmann in C++
+//              as a in-depth C++ project.
+//              Adapted from Christos Tsotskas's LBM code
+//              (itself adapted from a Fortran code by Prof. Djenidi)
+// Input: LBM_configuration.txt, LBM_geometry.txt
+// Output: geometry, convergence, report, data.
+//         Output files start with "LBM_"
+// ================================================
+
+// Possible CUDA extensions
+// #include "global_defines.cuh"
+
+using namespace std;
+
+int main() {
+	//cuda_device_query();
+	int lx = 1000, ly = 1000, lz = 1000, nb_densities = 100;
+
+}
+
