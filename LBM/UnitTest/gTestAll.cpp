4b964a48272e16b47f1c3a2b3b30d650611e82b4
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2016-12-01 11:15:01 +0000

    Set some unit tests.

---
+++ LBM/UnitTest/gTestAll.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file gTestAll.cpp
+ * @date 1 Dec 2016
+ * @author: RolandGuichard
+ */
+
+
+#include <stdio.h>
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+
